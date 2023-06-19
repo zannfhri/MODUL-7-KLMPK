a6e17f6bc8646a099b3924557deceb26fdbcd81a
zannfhri <124853019+zannfhri@users.noreply.github.com>
2023-06-19 20:33:46 +0700

    Delete storage directory

--- storage/framework/views/00fb3c523b2d0b54a91cc50d3ebda867.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Arthur File\ARTHUR FILE\Sem 4\framwork\Pengenalan Github\tugas-kelompok02\framwork-kelompok02\resources\views/profile.blade.php ENDPATH**/ ?>
--- storage/framework/views/dd3afef1ebc73859ac917e8eef0d8927.php
+++
@@ -1,9 +0,0 @@
-<div class="container mt-4">
-    <h4><?php echo e($pageTitle); ?></h4>
-    <hr>
-    <div class="d-flex align-items-center py-2 px-4 bg-light rounded-3 border">
-        <div class="bi-house-fill me-3 fs-1"></div>
-        <h4 class="mb-0">Well done! this is <?php echo e($pageTitle); ?>.</h4>
-    </div>
-</div>
-<?php /**PATH D:\Arthur File\ARTHUR FILE\Sem 4\framwork\Pengenalan Github\tugas-kelompok02\framwork-kelompok02\resources\views/default.blade.php ENDPATH**/ ?>
